a4b3f42210a53309c6d7014803044ed83d4c2e7a
Giymo11 <gizmo.head@yahoo.de>
2016-01-17 05:18:40 +0100

    more debugging

--- src/main/scala/rip/hansolo/http4s/Api.scala
+++ src/main/scala/rip/hansolo/http4s/Api.scala
@@ -1,11 +1,20 @@
 package rip.hansolo.http4s
 
+import org.http4s.Response
+import org.http4s.server.{Service, HttpService}
 import rip.hansolo.http4s.service._
 import rip.hansolo.http4s.service.special._
 
 import org.http4s.server.blaze.BlazeBuilder
 
+import scalaz.concurrent.Task
+
 object Api extends App {
+  import org.http4s.dsl._
+  val lifted = HttpService.lift { req => Task.now{
+    println(req.uri.authority)
+    Response(Ok)}
+  }
 
   // the 0.0.0.0 enables it to be picked up from outside
   BlazeBuilder.bindHttp(80, "0.0.0.0")
@@ -16,6 +25,7 @@ object Api extends App {
     .mountService(WsService(), "/ws")
     .mountService(TellMeService(), "/info")
     .mountService(HomeService(), "/home")
+    .mountService(lifted, "/lifted")
     .run
     .awaitShutdown()
 
--- src/main/scala/rip/hansolo/http4s/service/TellMeService.scala
+++ src/main/scala/rip/hansolo/http4s/service/TellMeService.scala
@@ -12,38 +12,36 @@ object TellMeService {
 
   private val service = HttpService {
     case req @ GET -> Root / "authority" => {
-      println(req.uri.authority)
-      Ok("" + req.uri.authority)
-    }
 
-    case req @ GET -> Root / "params" => {
-      println(req.uri.params)
-      Ok("" + req.uri.params)
-    }
+      val uri = req.uri
 
-    case req @ GET -> Root / "raw" => {
-      val request = req
-      println("remoteHost: " + req.remoteHost.get)
-      println("pathInfo: " + req.pathInfo)
-      println("serverAddr: " + req.serverAddr)
-      println("queryString: " + req.queryString)
+      println("uri host: " + uri.host)
+      println("uri host: " + uri.host.map(_.value))
 
-      println(req.multiParams.iterator.map(x => x._1 + ": " + x._2.fold("")(_ + " " + _)))
+      val authority = uri.authority
+      val host = authority.map(_.host)
+      val userInfo = authority.flatMap(_.userInfo).map(_.toString)
 
-      val attributes = req.attributes
-      for(i <- attributes.keys) {
-        val value = attributes.get(i)
-        println("" + i + ": " + value)
-      }
+      println("authority: " + authority)
+      println("host: " + host)
+      println("userInfo: " + userInfo)
+
       Ok()
     }
 
-    case req @ GET -> Root / "uri" => {
-      val uri = req.uri
-      println("host: " + uri.host.get)
-      uri.fragment.foreach(println)
-      println(uri.query.toVector.map((tuple) => "" + tuple._1 + ": " + tuple._2.getOrElse("")))
-      uri.host.foreach(println)
+
+    case req @ GET -> Root / "attributes" => {
+      val request = req
+      println("remoteHost: " + req.remoteHost.get)
+      println("serverAddr: " + req.serverAddr)
+      println("toString: " + req.toString)
+
+      println("attributes: ")
+      val attributes = req.attributes
+      for(key <- attributes.keys) {
+        val value = attributes.get(key)
+        println("" + key + ": " + value)
+      }
       Ok()
     }
 
