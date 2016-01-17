7578eadd4a60af042e4bcc251d6375816ec07a8d
Giymo11 <gizmo.head@yahoo.de>
2016-01-17 04:07:50 +0100

    adds some debug output to the info service

--- src/main/scala/rip/hansolo/http4s/service/TellMeService.scala
+++ src/main/scala/rip/hansolo/http4s/service/TellMeService.scala
@@ -21,6 +21,32 @@ object TellMeService {
       Ok("" + req.uri.params)
     }
 
+    case req @ GET -> Root / "raw" => {
+      val request = req
+      println("remoteHost: " + req.remoteHost.get)
+      println("pathInfo: " + req.pathInfo)
+      println("serverAddr: " + req.serverAddr)
+      println("queryString: " + req.queryString)
+
+      println(req.multiParams.iterator.map(x => x._1 + ": " + x._2.fold("")(_ + " " + _)))
+
+      val attributes = req.attributes
+      for(i <- attributes.keys) {
+        val value = attributes.get(i)
+        println("" + i + ": " + value)
+      }
+      Ok()
+    }
+
+    case req @ GET -> Root / "uri" => {
+      val uri = req.uri
+      println("host: " + uri.host.get)
+      uri.fragment.foreach(println)
+      println(uri.query.toVector.map((tuple) => "" + tuple._1 + ": " + tuple._2.getOrElse("")))
+      uri.host.foreach(println)
+      Ok()
+    }
+
 
   }
 }
