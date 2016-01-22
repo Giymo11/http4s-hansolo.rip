810ea0fa8cfc1fb7178604574ad625b8c76be1f3
Giymo11 <gizmo.head@yahoo.de>
2016-01-22 01:54:04 +0100

    implements the domain filter as a Middleware

--- src/main/scala/rip/hansolo/http4s/Api.scala
+++ src/main/scala/rip/hansolo/http4s/Api.scala
@@ -1,22 +1,56 @@
 package rip.hansolo.http4s
 
-import org.http4s.Response
+import org.http4s.headers.Host
+import org.http4s.{Request, Response}
 import org.http4s.server._
+import org.log4s._
 import rip.hansolo.http4s.service._
 import rip.hansolo.http4s.service.special._
 
 import org.http4s.server.blaze.BlazeBuilder
 
-import scalaz.concurrent.Task
+import scalaz._
 
 object Api extends App {
+
+  private[this] val logger = getLogger
+
   import org.http4s.dsl._
-  val lifted = HttpService.lift { req => Task.now{
-    println(req.uri.authority)
-    Response(Ok)}
+
+  /**
+    * Checks if the Host String fulfills the predicate
+    *
+    * @param predicate The function the domain is tested for
+    * @return The transformed service returning NotFound if the host does not satisfy the predicate
+    */
+  def domainFilter(predicate: String => Boolean): HttpMiddleware = Middleware[Request, Response, Request, Response] {
+    (req, service) => {
+      val uriHost: Option[String] = req.uri.host.map(_.value)
+      val headerHost: Option[String] = req.headers.get(Host).map(_.value)
+      val hostString: Option[String] = uriHost.orElse(headerHost)
+
+      import scalaz.syntax.std.option._
+
+      val hostDisjunction = for {
+        hostFound <- hostString \/> "no domain found in request"
+        hostValid <- Option(hostFound).filter(predicate) \/> "host is not satisfying predicatie"
+      } yield hostValid
+
+      hostDisjunction match {
+        case -\/(message) =>
+          println(message)
+          NotFound()
+        case \/-(host) =>
+          println("domain is passing")
+          service.run(req)
+      }
+    }
   }
 
-
+  /**
+    * A filter for domains used for testing
+    */
+  val testingDomainFilter = domainFilter((domain: String)  => domain.contains("test") || domain.contains("localhost"))
 
   // the 0.0.0.0 enables it to be picked up from outside
   BlazeBuilder.bindHttp(80, "0.0.0.0")
@@ -25,9 +59,8 @@ object Api extends App {
     .mountService(ProductService(), "/products")
     .mountService(StreamingService(), "/streaming")
     .mountService(WsService(), "/ws")
-    .mountService(TellMeService(), "/info")
+    .mountService(testingDomainFilter(TellMeService()), "/info")
     .mountService(HomeService(), "/home")
-    .mountService(lifted, "/lifted")
     .run
     .awaitShutdown()
 
--- src/main/scala/rip/hansolo/http4s/service/TellMeService.scala
+++ src/main/scala/rip/hansolo/http4s/service/TellMeService.scala
@@ -1,41 +1,16 @@
 package rip.hansolo.http4s.service
 
-import org.http4s.{Response, Request, HeaderKey}
 import org.http4s.dsl._
-import org.http4s.headers.Host
 import org.http4s.server.HttpService
 
-import scalaz.concurrent.Task
 
 /**
   * Created by Giymo11 on 2016-01-13 at 12:37.
   */
 object TellMeService {
 
-  // probably should do this with middleware
-  // https://github.com/http4s/http4s/blob/master/docs/src/test/scala/org/http4s/docs/CompositionExample.scala
   def apply(): HttpService = HttpService {
-    case req @ GET -> Root / _ => {
-      println("hello")
-      val uriHost = req.uri.host.map(_.value)
-      val headerHost = req.headers.get(Host).map(_.value)
-      println(uriHost)
-      println(headerHost)
-      val hostString = uriHost.orElse(headerHost)
-      if (hostString.isDefined &&
-         (hostString.get.contains("test") || hostString.get.contains("localhost")) ) {
-        println("handling")
-        handle(req)
-      } else {
-        println("not found: " + hostString)
-        NotFound()
-      }
-    }
-  }
-
-  private def handle: PartialFunction[Request, Task[Response]] = {
-    case req @ GET -> Root / "authority" => {
-
+    case req @ GET -> Root / "authority" =>
       val uri = req.uri
 
       println("uri host: " + uri.host)
@@ -50,11 +25,8 @@ object TellMeService {
       println("userInfo: " + userInfo)
 
       Ok()
-    }
 
-
-    case req @ GET -> Root / "attributes" => {
-
+    case req @ GET -> Root / "attributes" =>
       println("remoteHost: " + req.remoteHost.get)
       println("serverAddr: " + req.serverAddr)
       println("toString: " + req.toString)
@@ -66,13 +38,6 @@ object TellMeService {
         println("" + key + ": " + value)
       }
 
-      import org.http4s.headers.Host
-      val hostString = req.headers.get(Host).map(_.value)
-      println("host: " + hostString)
-
-      Ok(hostString.getOrElse(""))
-    }
-
-
+      Ok()
   }
 }
