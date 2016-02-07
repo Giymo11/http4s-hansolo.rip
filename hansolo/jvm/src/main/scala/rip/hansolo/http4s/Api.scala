6b031b354c48d34f6187df6c4865d61a74c7f7d9
Giymo11 <gizmo.head@yahoo.de>
2016-02-07 04:11:43 +0100

    Embeds the gdoc as an intermediate solution for the blog :P

--- hansolo/jvm/src/main/scala/rip/hansolo/http4s/Api.scala
+++ hansolo/jvm/src/main/scala/rip/hansolo/http4s/Api.scala
@@ -1,6 +1,10 @@
 package rip.hansolo.http4s
 
-import org.http4s.headers.Host
+import java.net.InetSocketAddress
+import java.util.concurrent.ExecutorService
+
+import org.http4s.MediaType._
+import org.http4s.headers.{`Content-Type`, Host}
 import org.http4s.server._
 import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.server.staticcontent.ResourceService
@@ -8,6 +12,7 @@ import org.http4s._
 import rip.hansolo.http4s.service._
 
 import scalaz._
+import scalaz.concurrent.Task
 
 object Api extends App {
 
@@ -26,6 +31,8 @@ object Api extends App {
       val hostString: Option[String] = uriHost.orElse(headerHost)
 
       import scalaz.syntax.std.option._
+
+      //val combinedPredicate = (domain: String) => domain.contains("localhost") || predicate(domain)
 
       val hostDisjunction = for {
         hostFound <- hostString \/> "no domain found in request"
@@ -47,21 +54,32 @@ object Api extends App {
     * A filter for domains used for testing
     */
   val testingDomainFilter = domainFilter((domain: String)  => domain.contains("test") || domain.contains("localhost"))
+  val tilDomainFilter = domainFilter((domain: String)  => domain.contains("til") || domain.contains("blog"))
 
   import staticcontent.resourceService
   import ResourceService.Config
 
   val redirectToKoiService = HttpService { case GET -> Root => SeeOther(Uri.fromString("http://koi.moe").getOrElse(Uri())) }
+  val gdocsService = HttpService { case GET -> Root =>
+    Ok(<html>
+          <body style="margin: 0">
+            <iframe style="width: 100%; height: 100%; border: 0; margin: 0"
+            src="https://docs.google.com/document/d/1EKExnRpNokVGlOvYpKQA2COOrITBlywvgANAN9qNnEE/pub?embedded=true"></iframe>
+          </body>
+        </html>.toString())
+      .withContentType(Some(`Content-Type`(`text/html`))) }
+
 
   // the 0.0.0.0 enables it to be picked up from outside
   BlazeBuilder.bindHttp(80, "0.0.0.0")
-    .mountService(MainPageService(), "/")
+    .mountService(tilDomainFilter(gdocsService), "/")
     .mountService(testingDomainFilter(TellMeService()), "/info")
     .mountService(HelloWorldService(), "/hello")
     .mountService(GithubWebhookService(), "/webhook")
     .mountService(GameService(), "/game")
     .mountService(resourceService(Config(basePath = "")), "/public")
     .mountService(redirectToKoiService, "/moe")
+    .mountService(MainPageService(), "/")
     .run
     .awaitShutdown()
 
