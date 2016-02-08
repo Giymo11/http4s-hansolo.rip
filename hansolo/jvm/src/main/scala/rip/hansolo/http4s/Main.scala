5dc01ab34ccb6b155ed886712e10c79500c53723
Giymo11 <gizmo.head@yahoo.de>
2016-02-08 14:42:31 +0100

    Cleans up the Main.scala

--- hansolo/jvm/src/main/scala/rip/hansolo/http4s/Main.scala
+++ hansolo/jvm/src/main/scala/rip/hansolo/http4s/Main.scala
@@ -1,46 +1,42 @@
 package rip.hansolo.http4s
 
 import org.http4s._
+import org.http4s.dsl._
 import org.http4s.server._
 import org.http4s.server.syntax._
+import org.http4s.server.staticcontent._
+import org.http4s.server.staticcontent.ResourceService._
 import org.http4s.server.blaze.BlazeBuilder
 
 import rip.hansolo.http4s.service._
+import rip.hansolo.http4s.dsl.HttpServiceOps._
 
 import scalaz._
 
-object Api extends App {
+object Main extends App {
 
-  val staticResourceService = {
-    import server.staticcontent._
-    import staticcontent.ResourceService._
-    resourceService(Config(basePath = ""))
-  }
-  val redirectToKoiService = {
-    import org.http4s.dsl._
-    HttpService.lift(req => SeeOther(Uri.fromString("http://koi.moe").getOrElse(Uri())))
-  }
+  val staticResourceService = resourceService(Config(basePath = ""))
+  val redirectToKoiService = HttpService.lift(req => SeeOther(Uri.fromString("http://koi.moe").getOrElse(Uri())))
 
 
   type Path = String
   type MountingMap = Map[Path, NonEmptyList[HttpService]]
 
   /** the map with all the mounting points. The first service not returning a fallthrough response will be used. */
-  val mountingMap: MountingMap = {
-    import dsl.HttpServiceOps._
-    Map(
+  val mountingMap: MountingMap = Map(
       "/" -> NonEmptyList(TilService().forDomain("blog"), MainPageService()),
       "/info" -> NonEmptyList(TellMeService().forDomains(Seq("test", "localhost"))),
       "/hello" -> NonEmptyList(HelloWorldService())
     )
-  }
+
   def mapMountpointsOntoBuilder(initialBuilder: ServerBuilder, mountingMap: MountingMap): ServerBuilder = {
     // fold over all entries in the mountingmap, using the initialBuilder as a start vale
     mountingMap.foldLeft(initialBuilder)(
       (builder, entry) => {
         val (path, services) = entry
-        import scalaz.syntax.foldable1._
+
         // fold the services together, in that if the first returns HttpService.notFound, the second will be tried, and so on
+        import scalaz.syntax.foldable1._
         builder.mountService(services.foldLeft1(_ || _), path)
       }
     )
