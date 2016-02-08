ccc40cac68e20c5f2bb6f0610678b815197c2c7f
Giymo11 <gizmo.head@yahoo.de>
2016-02-08 04:51:47 +0100

    Fixes the fallthrough for the the domainFilter, you can now define a map of services to mount on a path and it will try all before returning 404 for that path.

--- hansolo/jvm/src/main/scala/rip/hansolo/http4s/Api.scala
+++ hansolo/jvm/src/main/scala/rip/hansolo/http4s/Api.scala
@@ -1,18 +1,16 @@
 package rip.hansolo.http4s
 
-import java.net.InetSocketAddress
-import java.util.concurrent.ExecutorService
-
-import org.http4s.MediaType._
-import org.http4s.headers.{`Content-Type`, Host}
+import org.http4s._
 import org.http4s.server._
+import org.http4s.server.syntax._
 import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.server.staticcontent.ResourceService
-import org.http4s._
+import org.http4s.MediaType._
+import org.http4s.headers.{`Content-Type`, Host}
+
 import rip.hansolo.http4s.service._
 
 import scalaz._
-import scalaz.concurrent.Task
 
 object Api extends App {
 
@@ -32,8 +30,6 @@ object Api extends App {
 
       import scalaz.syntax.std.option._
 
-      //val combinedPredicate = (domain: String) => domain.contains("localhost") || predicate(domain)
-
       val hostDisjunction = for {
         hostFound <- hostString \/> "no domain found in request"
         hostValid <- (Option(hostFound) filter predicate) \/> "host is not satisfying predicate"
@@ -42,7 +38,9 @@ object Api extends App {
       hostDisjunction match {
         case -\/(message) =>
           println(message)
-          NotFound()
+          HttpService.notFound
+        // if you use this instead of just NotFound(), it attaches a key to the attributes of the response,
+        // enabling it to fall through to another service via Service.withFallthrough
         case \/-(host) =>
           println("domain is passing")
           service.run(req)
@@ -59,27 +57,46 @@ object Api extends App {
   import staticcontent.resourceService
   import ResourceService.Config
 
-  val redirectToKoiService = HttpService { case GET -> Root => SeeOther(Uri.fromString("http://koi.moe").getOrElse(Uri())) }
-  val gdocsService = HttpService { case GET -> Root =>
+  val redirectToKoiService = HttpService.lift(req => SeeOther(Uri.fromString("http://koi.moe").getOrElse(Uri())))
+  val gdocsService = HttpService.lift( req =>
     Ok(<html>
           <body style="margin: 0">
             <iframe style="width: 100%; height: 100%; border: 0; margin: 0"
             src="https://docs.google.com/document/d/1EKExnRpNokVGlOvYpKQA2COOrITBlywvgANAN9qNnEE/pub?embedded=true"></iframe>
           </body>
-        </html>.toString())
-      .withContentType(Some(`Content-Type`(`text/html`))) }
+        </html>.toString()
+    ).withContentType(Some(`Content-Type`(`text/html`)))
+  )
 
+  type Path = String
+
+  // the map with all the mounting points. The first service not returning a fallthrough response will be used.
+  val mountingMap: Map[Path, NonEmptyList[HttpService]] = Map(
+    "/" -> NonEmptyList(tilDomainFilter(gdocsService), MainPageService()),
+    "/info" -> NonEmptyList(testingDomainFilter(TellMeService())),
+    "/hello" -> NonEmptyList(HelloWorldService())
+  )
+
+  def mapMountpointsOntoBuilder(initialBuilder: ServerBuilder, mountingMap: Map[Path, NonEmptyList[HttpService]]) = {
+    // fold over all entries in the mountingmap, using the initialBuilder as a start vale
+    mountingMap.foldLeft(initialBuilder)(
+      (builder, entry) => {
+        val (path, services) = entry
+        import scalaz.syntax.foldable1._
+        // fold the services together, in that if the first returns HttpService.notFound, the second will be tried, and so on
+        builder.mountService(services.foldLeft1(_ || _), path)
+      }
+    )
+  }
 
   // the 0.0.0.0 enables it to be picked up from outside
-  BlazeBuilder.bindHttp(80, "0.0.0.0")
-    .mountService(tilDomainFilter(gdocsService), "/")
-    .mountService(testingDomainFilter(TellMeService()), "/info")
-    .mountService(HelloWorldService(), "/hello")
+  val builder: ServerBuilder = mapMountpointsOntoBuilder(BlazeBuilder.bindHttp(80, "0.0.0.0"), mountingMap)
+
+  builder
     .mountService(GithubWebhookService(), "/webhook")
     .mountService(GameService(), "/game")
     .mountService(resourceService(Config(basePath = "")), "/public")
     .mountService(redirectToKoiService, "/moe")
-    .mountService(MainPageService(), "/")
     .run
     .awaitShutdown()
 
