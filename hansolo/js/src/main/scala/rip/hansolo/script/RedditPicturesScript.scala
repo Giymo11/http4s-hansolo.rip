91a6383ce89d00385bf80934abc642da99949f56
Giymo11 <gizmo.head@yahoo.de>
2016-02-09 18:03:50 +0100

    First alien contact!

--- hansolo/js/src/main/scala/rip/hansolo/script/RedditPicturesScript.scala
+++ hansolo/js/src/main/scala/rip/hansolo/script/RedditPicturesScript.scala
@@ -4,11 +4,16 @@ import org.scalajs.dom
 import rx.async.Timer
 import rx.{Ctx, Rx, Var}
 
-import scala.scalajs.js.JSApp
+import scala.scalajs.js
+import scala.scalajs.js.{JSON, JSApp}
 import scala.scalajs.js.annotation.JSExport
 import scalatags.JsDom.all._
 
 import ScalatagsRxImplicits._
+
+import dom.ext.Ajax
+
+import js.JSConverters._
 
 /**
   * Created by Giymo11 on 09.02.2016.
@@ -18,11 +23,12 @@ object RedditPicturesScript extends JSApp {
   val secondsPassed = Var(0)
   val helloWorld = Rx("secondsPassed: " + secondsPassed())
   val timer = {
-    import Ctx.Owner.Unsafe._
     import rx.async.Platform._
     import scala.concurrent.duration._
     Timer(1 second)
   }
+
+  val redditResponse: Var[Frag] = Var(p("No Response Yet"))
 
   @JSExport
   override def main(): Unit = {
@@ -30,11 +36,32 @@ object RedditPicturesScript extends JSApp {
 
     timer triggerLater {secondsPassed() = secondsPassed.now + 1}
 
+    val testSubredditUrl = "https://www.reddit.com/r/reddit_api_test.json"
+
+    val startAjax = () => {
+      import scala.concurrent.ExecutionContext.Implicits.global
+      val future = Ajax.get(testSubredditUrl)
+      future.onSuccess { case xhr =>
+        val json = JSON.parse(xhr.responseText)
+
+        val posts: Seq[js.Dynamic] = json.data.children.asInstanceOf[js.Array[js.Dynamic]]
+        val titles = posts.map(child => child.data.title.asInstanceOf[String])
+
+        println("worked?")
+        redditResponse() = p("Kind: " + json.kind + ", first child: " + titles.head)
+      }
+      future.onFailure { case e => redditResponse() = "Error: " + e.getMessage}
+    }
+
     val body = dom.document.body
     body.innerHTML = ""
     body.appendChild(
-      p(helloWorld).render
+      div(id := "scalatags",
+        p(helloWorld),
+        button("start ajax", onclick := startAjax),
+        redditResponse
+      ).render
     )
-  }
 
+  }
 }
