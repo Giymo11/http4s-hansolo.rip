35f3416e0b0a53678b6859b4b41e34ac2fa37841
Giymo11 <gizmo.head@yahoo.de>
2016-02-10 01:31:30 +0100

    cleaned up all that Rx mess

--- hansolo/js/src/main/scala/rip/hansolo/script/RedditPicturesScript.scala
+++ hansolo/js/src/main/scala/rip/hansolo/script/RedditPicturesScript.scala
@@ -1,21 +1,21 @@
 package rip.hansolo.script
 
-import org.scalajs.dom
-import org.scalajs.dom.raw.Event
-import rx.async.Timer
-import rx.{Ctx, Rx, Var}
+import scala.concurrent.{ExecutionContext, Future}
 
-import scala.concurrent.Future
 import scala.scalajs.js
 import scala.scalajs.js.{JSON, JSApp}
 import scala.scalajs.js.annotation.JSExport
+
+import org.scalajs.dom
+import org.scalajs.dom.raw.Event
+import org.scalajs.dom.ext.Ajax
+
 import scalatags.JsDom.all._
 
+import rx._
+import rx.async._
+
 import ScalatagsRxImplicits._
-
-import dom.ext.Ajax
-
-import js.JSConverters._
 
 /**
   * Created by Giymo11 on 09.02.2016.
@@ -30,34 +30,36 @@ object RedditPicturesScript extends JSApp {
     Timer(1 second)
   }
 
-  val redditResponse: Var[Frag] = Var(p("No Response Yet"))
+  val redditUrl = "https://www.reddit.com"
+  val subredditUrl = Var("/r/reddit_api_test.json")
 
-  @JSExport
-  override def main(): Unit = {
-    println("Hello?")
+  /**
+    * The input field to put enter the query
+    */
+  val queryInput = input(
+    id := "query-input",
+    placeholder := subredditUrl,
+    autofocus := true
+  ).render
 
-    timer triggerLater {secondsPassed() = secondsPassed.now + 1}
+  // onchange is fired e.g. when pressing enter or losing focus
+  queryInput.onchange = (e: Event) => {
+    subredditUrl() = queryInput.value
+    queryInput.value = ""
+  }
 
-    val redditUrl = "https://www.reddit.com"
-    val subredditUrl = Var("/r/reddit_api_test.json")
+  /**
+    * The Frags sent as Response to the JSON request.
+    * Depends on subredditUrl.
+    */
+  val responseRx = {
+    import scala.concurrent.ExecutionContext.Implicits.global
 
-    val queryInput = input(
-      id := "query-input",
-      placeholder := subredditUrl,
-      autofocus := true
-    ).render
-
-    queryInput.onchange = (e: Event) => {
-      subredditUrl() = queryInput.value
-      queryInput.value = ""
-    }
-
-    val startAjax = () => {
-      import scala.concurrent.ExecutionContext.Implicits.global
-
-      val testSubredditUrl = redditUrl + subredditUrl.now
-
-      val future = Ajax.get(testSubredditUrl)
+    /**
+      * The future of finished Frags to put into the dom.
+      */
+    val futureRx = Rx[Future[Frag]] {
+      Ajax.get(redditUrl + subredditUrl())
         .map(xhr => JSON.parse(xhr.responseText))
         .map(json => {
           val posts: Seq[js.Dynamic] = json.data.children.asInstanceOf[js.Array[js.Dynamic]]
@@ -70,10 +72,18 @@ object RedditPicturesScript extends JSApp {
         }.recover { case e =>
           span(e.getMessage, backgroundColor := "red")
         }
-      future foreach (ele => redditResponse() = ele)
     }
 
-    subredditUrl foreach (newValue => startAjax())
+    // this should work, but is currently bugged. To be fixed in scala.rx 0.3.1
+    //futureRx.flatMap(future => futureToRx(future, p("fetching response for " + subredditUrl())))
+    futureRx.map(future => future.toRx(p("fetching response for " + subredditUrl.now)))
+  }
+
+  @JSExport
+  override def main(): Unit = {
+    println("Hello?")
+
+    timer triggerLater {secondsPassed() = secondsPassed.now + 1}
 
     val body = dom.document.body
     body.innerHTML = ""
@@ -82,8 +92,7 @@ object RedditPicturesScript extends JSApp {
         p(helloWorld),
         queryInput,
         p(subredditUrl),
-        button("start ajax", onclick := startAjax),
-        redditResponse
+        responseRx
       ).render
     )
   }
